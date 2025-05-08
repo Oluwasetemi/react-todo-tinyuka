fee4b217fb18326fcdd888eb44f1ce9f9947ff80
oluwasetemi ojo <setemiojo@gmail.com>
2025-05-08 12:13:45 -0500

    👌 IMPROVE: automatically import react with esbuild.jsxInject

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import Greeting from './components/Greeting';
+import Greeting from "./components/Greeting";
 
 export default function App() {
   return (
     <div className="flex items-center border-2 border-green-500 h-screen">
       <Greeting />
-      <h1 className="text-red-500">Hello</h1><p>World</p>
-      <img className="p-2 w-[200px] h-[200px] rounded-full rounded-t-full" src={`https://res.cloudinary.com/drnqdd87d/image/upload/v1730477919/headshot/jmvng5pooqv8fc0o7mr7.jpg`} alt={`alt image of the tutor`} />
+      <h1 className="text-red-500">Hello</h1>
+      <p>World</p>
+      <img
+        className="p-2 w-[200px] h-[200px] rounded-full rounded-t-full"
+        src={`https://res.cloudinary.com/drnqdd87d/image/upload/v1730477919/headshot/jmvng5pooqv8fc0o7mr7.jpg`}
+        alt={`alt image of the tutor`}
+      />
     </div>
-  )
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import "./style.css";
-import React from "react";
+// import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Form from "./components/form.jsx";
