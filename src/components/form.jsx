5c936544061a466f6134c609fd78dd8ca9006bed
Mariam Oluseyi-Alli <mariam_oa@yahoo.com>
2025-05-09 08:45:30 +0100

    {commit_message}

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,6 @@
 export default function Form({ createTodo, setText, text }) {
   return (
-    <form className="max-w-[600px] m-[auto] flex flex-col items-center bg-navyblue text-gray-200" onSubmit={createTodo}>
+    <form className="max-w-[600px] m-[auto] flex flex-col items-center bg-navyblue text-gray-200 border-gray-500" onSubmit={createTodo}>
       <h1 className="text-2xl text-center font-semibold mt-[2rem] mb-[1rem]">Todo App</h1>
       <input
         className="p-2 border-2 rounded placeholder:capitalize w-full max-w-[400px] text-center text-gray-600"
